fe9d1bea022bb0da5ef8914feb3a2506beaf4a33
Almendra Romina R <almendraromina1@gmail.com>
2024-01-06 22:44:46 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TextGlitch from './Components/TextGlitch/TextGlitch'
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <figure className='banner'>
+
+        </figure>
+        <img className='logo' src="" alt="logo de la zorreria" />
+
+        <h1>La Zorreria</h1>
+        <p>El furuto es zorre</p>
+      </header>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/TextGlitch/TextGlitch.jsx
@@ -0,0 +1,11 @@
+import './TextGlitch.css'
+
+const TextGlitch = ( text) => {
+  return (
+    <div>
+      <h2 className="glitch" data-text="El futuro es zorre">El futuro es zorre</h2>
+    </div>
+  )
+}
+
+export default TextGlitch
